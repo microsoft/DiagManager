78f4fb0ca88f1969d298b18920c576e4757599cd
jopilov@microsoft.com <joseph.pilov@microsoft.com>
2021-06-09 16:37:38 -0500

    Update of file version

--- DiagManager/Properties/AssemblyInfo.cs
+++ DiagManager/Properties/AssemblyInfo.cs
@@ -67,5 +67,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("15.21.5.25")]
-[assembly: AssemblyFileVersion("15.21.5.25")]
+[assembly: AssemblyVersion("15.21.06.09")]
+[assembly: AssemblyFileVersion("15.21.06.09")]
