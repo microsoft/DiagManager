a520dd3fd0726bfcf0b9f7d63884d9ca85effeb4
PijoCoder <23519517+PiJoCoder@users.noreply.github.com>
2021-09-08 16:20:05 -0500

    More modifications to QueryNeverCompletes T-SQL script

--- DiagManager/CustomDiagnostics/SQL Server High CPU Perf Stats/QueryNeverCompletes_perfstats.sql
+++ DiagManager/CustomDiagnostics/SQL Server High CPU Perf Stats/QueryNeverCompletes_perfstats.sql
@@ -1,37 +1,30 @@
-
+use tempdb
+go
 IF OBJECT_ID ('sp_perf_never_ending_query_snapshots','P') IS NOT NULL
    DROP PROCEDURE sp_perf_never_ending_query_snapshots
 GO
 
-CREATE PROCEDURE sp_perf_never_ending_query_snapshots @appname sysname='PSSDIAG', @runtime datetime, @runtime_utc datetime
-as
+CREATE PROCEDURE sp_perf_never_ending_query_snapshots @appname sysname='PSSDIAG'
+AS
+SET NOCOUNT ON
 
-set nocount on 
+DECLARE @cpu_threshold_ms int = 60000
+
 BEGIN
  DECLARE @msg varchar(100)
 
- IF NOT EXISTS (SELECT * FROM sys.dm_exec_requests req left outer join sys.dm_exec_sessions sess
+ IF EXISTS (SELECT * FROM sys.dm_exec_requests req left outer join sys.dm_exec_sessions sess
 				on req.session_id = sess.session_id
-				WHERE req.session_id <> @@SPID AND ISNULL (sess.host_name, '') != @appname and is_user_process = 1) 
-  BEGIN
-    PRINT 'No active queries'
-  END
- ELSE 
-  BEGIN
-
---  select '' 
-
-    IF @runtime IS NULL or @runtime_utc IS NULL
-      BEGIN 
-        SET @runtime = GETDATE()
-		SET @runtime_utc = GETUTCDATE()
-        --SET @msg = 'Start time: ' + CONVERT (varchar(30), @runtime, 126)
-        RAISERROR (@msg, 0, 1) WITH NOWAIT
-      END
-
+				WHERE req.session_id <> @@SPID AND ISNULL (sess.host_name, '') != @appname and sess.is_user_process = 1 AND req.cpu_time > @cpu_threshold_ms) 
+					
+ BEGIN
+    
+	DECLARE @runtime datetime = GETDATE(), @runtime_utc datetime = GETUTCDATE()
+    --SET @msg = 'Start time: ' + CONVERT (varchar(30), @runtime, 126)
+    RAISERROR (@msg, 0, 1) WITH NOWAIT
 
 	
-	print ''
+	PRINT ''
 	RAISERROR ('--  neverending_query --', 0, 1) WITH NOWAIT
 
            --query the DMV in a loop to compare the 
@@ -45,20 +38,25 @@ BEGIN
             qp.rewind_count,
             qp.rebind_count, end_of_scan_count,
             qp.estimate_row_count,
-            er.cpu_time,
-            er.total_elapsed_time
+            req.cpu_time,
+            req.total_elapsed_time
         FROM sys.dm_exec_query_profiles qp 
-		LEFT OUTER JOIN sys.dm_exec_requests er
-			ON qp.session_id = er.session_id
+		LEFT OUTER JOIN sys.dm_exec_requests req
+			ON qp.session_id = req.session_id
+		LEFT OUTER JOIN sys.dm_exec_sessions sess
+			on req.session_id = sess.session_id
 		CROSS APPLY sys.dm_exec_sql_text(qp.sql_handle) st
-		WHERE qp.session_id != @@SPID  
-			AND er.cpu_time > 60000
+		WHERE req.session_id <> @@SPID 
+			AND ISNULL (sess.host_name, '') != @appname 
+			AND sess.is_user_process = 1 
+			AND req.cpu_time > @cpu_threshold_ms 
         ORDER BY qp.node_id
 		--this is to prevent massive grants
 		OPTION (max_grant_percent = 3, MAXDOP 1)
     
 	--flush results to client
 	RAISERROR (' ', 0, 1) WITH NOWAIT
+
   END
 END
 GO
@@ -66,11 +64,31 @@ GO
 
 
 IF OBJECT_ID ('sp_Run_NeverEndingQuery_Stats','P') IS NOT NULL
-   DROP PROCEDURE sp_Run_NeverEndingQuery_Stats
+   DROP PROCEDURE dbo.sp_Run_NeverEndingQuery_Stats
 GO
 
-create procedure sp_Run_NeverEndingQuery_Stats
+CREATE PROCEDURE dbo.sp_Run_NeverEndingQuery_Stats
 as
+SET NOCOUNT ON
+
+PRINT 'starting query never seems to complete perf stats script...'
+set language us_english
+PRINT '-- script source --'
+select 'query never completes stats script' as script_name
+PRINT ''
+PRINT '-- script and environment details --'
+PRINT 'name                     value'
+PRINT '------------------------ ---------------------------------------------------'
+PRINT 'sql server name          ' + @@servername
+PRINT 'machine name             ' + convert (varchar, serverproperty ('machinename'))
+PRINT 'sql version (sp)         ' + convert (varchar, serverproperty ('productversion')) + ' (' + convert (varchar, serverproperty ('productlevel')) + ')'
+PRINT 'edition                  ' + convert (varchar, serverproperty ('edition'))
+PRINT 'script name              Query Never Completes stats script'
+PRINT 'script file name         $file: QueryNeverCompletes_perfstats.sql $'
+PRINT 'last modified            $date: 2021/09/07  $'
+PRINT 'script begin time        ' + convert (varchar(30), getdate(), 126) 
+PRINT 'current database         ' + db_name()
+PRINT '@@spid                   ' + ltrim(str(@@spid))
 
 
 --handle SQL Server 2008 code line, thus need to parse ProductVersion
@@ -79,7 +97,8 @@ SET @servermajorversion = CONVERT (INT, (REPLACE (LEFT (CONVERT (nvarchar, SERVE
 
 if (@servermajorversion < 12)
 begin
-    RAISERROR ('SQL Server version is less than 2014. No additional data can be collected', 0, 1) WITH NOWAIT
+    RAISERROR ('Lightweight Profiling    SQL Server version is less than 2014. No additional data can be collected', 0, 1) WITH NOWAIT
+	PRINT ''
     return
 end
 
@@ -92,46 +111,25 @@ SET @serverbuild = CONVERT (int, SERVERPROPERTY ('ProductBuild'))
 --minimum build 12.0.5000.0 , see https://docs.microsoft.com/en-us/sql/relational-databases/performance/query-profiling-infrastructure?view=sql-server-ver15
 if (@servermajorversion <= 12 and @serverbuild < 5000)
 begin
-    RAISERROR ('Your SQL Sever version does not support collecting real-time perf stats on long-running query', 0, 1) WITH NOWAIT
+    RAISERROR ('Lightweight Profiling    Your SQL Sever version does not support collecting real-time perf stats on long-running query', 0, 1) WITH NOWAIT
+	PRINT ''
 end
 --13.0.4001.0 (SP1)
 else if ((@servermajorversion = '13' and @serverbuild <4001) or (@servermajorversion = 12 and @serverbuild >= 5000))
 begin
-    RAISERROR ('Version SQL 2016 RTM (less than SP1) or SQL 2014 SP2+. Using Lightweight Profiling Ver1', 0, 1) WITH NOWAIT
-    --create the event 
+	RAISERROR ('Lightweight Profiling    Using Lightweight Profiling Ver1 requires that you enable SET STATISTICS PROFILE ON in the same session where the query runs', 0, 1) WITH NOWAIT
+	PRINT ''
+	PRINT 'See https://docs.microsoft.com/en-us/sql/relational-databases/system-dynamic-management-views/sys-dm-exec-query-profiles-transact-sql#examples for more information'
+	PRINT ''
 
-    CREATE EVENT SESSION [pssdiag_query_thread_profile] ON SERVER
-    ADD EVENT sqlserver.query_thread_profile(
-    ACTION(sqlos.scheduler_id,sqlserver.server_instance_name,sqlserver.session_id))
-    ADD TARGET package0.ring_buffer(SET max_memory=(25600))
-    WITH (MAX_MEMORY=4096 KB,
-    EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS,
-    MAX_DISPATCH_LATENCY=30 SECONDS,
-    MAX_EVENT_SIZE=0 KB,
-    MEMORY_PARTITION_MODE=NONE,
-    TRACK_CAUSALITY=OFF,
-    STARTUP_STATE=OFF);
-
-    ALTER EVENT SESSION [pssdiag_query_thread_profile] 
-    ON SERVER
-    STATE = START
-
-	--TODO:
-	-- TURN OFF THE XEVENT WHEN DONE - how do we hide checkbox in the GUI
-	-- ALSO RUN BATCH SCRIPT TO GET PLANS 
-
-    WHILE (1=1)
-	BEGIN
-        --query the DMV in a loop to compare the 
-		EXEC sp_perf_never_ending_query_snapshots @appname = 'PSSDIAG'
-        WAITFOR DELAY '00:00:10'
-    END
 
 end
 else if ((@servermajorversion = '13' and @serverbuild >=4001) or @servermajorversion = '14')
 begin
-    RAISERROR ('Version SQL 2016 SP1+ or 2017. Using Lightweight Profiling Ver2', 0, 1) WITH NOWAIT
-    --dbcc traceoff (7412, -1)
+    RAISERROR ('Lightweight Profiling    SQL 2016 SP1+ or 2017. Using Lightweight Profiling Ver2', 0, 1) WITH NOWAIT
+	PRINT ''
+	PRINT 'Enabling TF 7412'
+    DBCC TRACEON (7412, -1)
 
     WHILE (1=1)
 	BEGIN
@@ -143,7 +141,8 @@ begin
 end
 else if (@servermajorversion >= '15')
 begin
-    RAISERROR ('Version SQL 2019. Using Lightweight Profiling Ver3 (enabled by default)', 0, 1) WITH NOWAIT
+    RAISERROR ('Lightweight Profiling    SQL 2019. Using Lightweight Profiling Ver3 (enabled by default)', 0, 1) WITH NOWAIT
+	PRINT ''
 
 	WHILE (1=1)
 	BEGIN
@@ -154,4 +153,9 @@ begin
 end
 
 go
-exec sp_Run_NeverEndingQuery_Stats
+exec dbo.sp_Run_NeverEndingQuery_Stats
+
+
+
+	--TODO:
+	-- ALSO RUN BATCH SCRIPT TO GET PLANS 
