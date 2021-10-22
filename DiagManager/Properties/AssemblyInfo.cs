e4ebc47c2ed786a0b6c0724d3a946743b796a160
jopilov@microsoft.com <23519517+PiJoCoder@users.noreply.github.com>
2021-10-22 18:09:58 -0500

    Version Bump
    Co-Authored-By: Todd <22751647+gambit9009@users.noreply.github.com>

--- DiagManager/Properties/AssemblyInfo.cs
+++ DiagManager/Properties/AssemblyInfo.cs
@@ -67,5 +67,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("15.21.10.01")]
-[assembly: AssemblyFileVersion("15.21.10.01")]
+[assembly: AssemblyVersion("15.21.10.22")]
+[assembly: AssemblyFileVersion("15.21.10.22")]
