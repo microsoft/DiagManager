59bddf32a2d0a416dacfe529ac677e171ae20bf9
tiagodiasfernandes <tiago.fernandes@microsoft.com>
2021-11-03 20:24:11 +0000

    Batch file improvements
    Traceflag restore customer config
    Loop 3 times on startup execution

--- DiagManager/CustomDiagnostics/SQL Server High CPU Perf Stats/PlanGeneratingNeverEndingQuery.bat
+++ DiagManager/CustomDiagnostics/SQL Server High CPU Perf Stats/PlanGeneratingNeverEndingQuery.bat
@@ -16,7 +16,11 @@ REM
 set NL=^
 
 
-set query=SET NOCOUNT ON
+set query=IF (OBJECT_ID('tempdb.dbo.original_config_tf_7412')) IS NULL
+set query=!query!!NL!CREATE TABLE tempdb.dbo.original_config_tf_7412 ([ID] [bigint] IDENTITY(1,1) NOT NULL,[TraceFlag] INT, Status INT, Global INT, Session INT)
+set query=!query!!NL!INSERT INTO tempdb.dbo.original_config_tf_7412 EXEC('DBCC TRACESTATUS (7412)')
+set query=!query!!NL!IF EXISTS (SELECT 1 FROM tempdb.dbo.original_config_tf_7412 WHERE GLOBAL = 0 AND TraceFlag = 7412) DBCC TRACEON (7412, -1)
+set query=!query!!NL!SET NOCOUNT ON
 set query=!query!!NL!declare @starttime datetime = getdate(), @cnt int
 set query=!query!!NL!while (1=1)
 set query=!query!!NL!begin
@@ -36,7 +40,7 @@ set query=!query!!NL!end
 
 
 rem execute the query 
-sqlcmd -E -S%3 -Q"EXIT(!query!)"
+sqlcmd -E -S%2 -Q"EXIT(!query!)"
 
 REM If query batch returned -1 it means, the batch "timed out", didn't find any long-running CPU-bound queries
 IF %ERRORLEVEL% EQU 78787878 (
@@ -44,13 +48,17 @@ echo batch timeout out, no high-CPU queries found
 GOTO Exit
 )
 
-
 set /A cntr=0
-
 REM if the query batch returned more than 5 queries, we will only process 5, else use the actual number returned
-
-IF %ERRORLEVEL% GTR 5 (SET cntr=5) ELSE (SET cntr=%ERRORLEVEL%)
-for /L %%x in (1,1, %cntr%) do bcp "select xmlplan from (SELECT TOP %1 ROW_NUMBER() OVER(ORDER BY (cpu_time) DESC) AS RowNumber, query_plan AS xmlplan FROM sys.dm_exec_requests r CROSS APPLY sys.dm_exec_query_statistics_xml(session_id) WHERE r.cpu_time > 1) as x WHERE RowNumber =%%x" queryout "%2%%x.sqlplan" -T -c -S %3
-
-
+if %ERRORLEVEL% GTR 5 (
+	set /a cntr=5
+) else (
+	set /a cntr=%ERRORLEVEL%
+)
+for /L %%y in (1,1, 3) do (
+	for /L %%x in (1,1, %cntr%) do (
+		bcp "select xmlplan from (SELECT TOP %cntr% ROW_NUMBER() OVER(ORDER BY (r.cpu_time) DESC) AS RowNumber, x.query_plan AS xmlplan, t.text AS sql_text FROM sys.dm_exec_requests AS r INNER JOIN sys.dm_exec_sessions AS s ON r.session_id = s.session_id CROSS APPLY sys.dm_exec_sql_text(r.sql_handle) AS t CROSS APPLY sys.dm_exec_query_statistics_xml(r.session_id) AS x WHERE s.is_user_process = 1 AND r.cpu_time > 60000 ) as x WHERE RowNumber =%%x" queryout "%1_run%%y_plan%%x.sqlplan" -T -c -S %2
+	)
+	(echo %1 | findstr /i /c:"Startup" >nul) && (IF %%y LSS 3 (timeout 120)) || (GOTO Exit)
+)
 :Exit
