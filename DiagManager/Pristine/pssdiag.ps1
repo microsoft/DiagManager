dda1efa078e02dc7a8be9973b39151edb77f1956
asavioli <andre.savioli@outlook.com>
2022-09-05 18:11:42 +0100

    Update pssdiag.ps1
    Fix issue with -1 value returned by IndexOf when working with instances without minor build #188

--- DiagManager/Pristine/pssdiag.ps1
+++ DiagManager/Pristine/pssdiag.ps1
@@ -213,7 +213,15 @@ function ValidateCurrentVersion ([string]$ssver)
 		$majVersion = $verString.Substring(0, $verString.IndexOf("."))
 
         # for version 2008R2 number is 10.50 and we had to remove .50
-        $tempssver = $ssver.Substring(0, $ssver.IndexOf("."))
+        if ($ssver.IndexOf(".") -eq -1) #IndexOf function returns -1 for SQLs without minor version number (only 2008R2 has this number, which is 50. All others are zero). This fails on the Substring function.
+        {
+            $tempssver =  $ssver
+
+        }
+        else 
+        {
+            $tempssver = $ssver.Substring(0, $ssver.IndexOf("."))
+        }
 
 
 
