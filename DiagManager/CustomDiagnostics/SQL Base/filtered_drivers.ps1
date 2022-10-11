1fa571e7123496df0bce6ceb008fc9adcf74d20f
asavioli <andre.savioli@outlook.com>
2022-10-11 18:40:00 +0100

    Update filtered_drivers.ps1
    removing meaningless comments. Changing window style to hidden. Issue #199

--- DiagManager/CustomDiagnostics/SQL Base/filtered_drivers.ps1
+++ DiagManager/CustomDiagnostics/SQL Base/filtered_drivers.ps1
@@ -1,5 +1,3 @@
-# run example
-# powershell.exe -ExecutionPolicy Bypass   .\SqlBaseUtil.ps1 GetWindowsHotfix
 param(
   [string]$argument,
   [string]$output_path
@@ -11,12 +9,10 @@ try
 
     #filter drivers
     $executable = "fltmc.exe"
-    #throw 'abc'
-    Write-Host $output_path
-
+    
     if (($argument -eq "filters") -or ($argument -eq "instances"))
     {
-        Start-Process -FilePath $executable -ArgumentList $argument -WindowStyle Normal -RedirectStandardOutput $output_path
+        Start-Process -FilePath $executable -ArgumentList $argument -WindowStyle Hidden -RedirectStandardOutput $output_path
     }
 
 }
