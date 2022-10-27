1f0af78d2cbad02c4ee3dc2f949155b3369fdbaa
Kinye Yu <kennyyu2000@gmail.com>
2022-10-27 08:44:05 -0500

    Multicopy powershell cmd
    Created a new powershell command to replaced the cmd command

---
+++ DiagManager/CustomDiagnostics/SQL Base/Multicopy.ps1
@@ -0,0 +1,13 @@
+﻿param(
+  [string]$source_path,
+  [string]$destination_path,
+  [string]$file_name
+)
+
+ 
+$count = @(Get-ChildItem -Path $source_path -Filter $file_name).Count
+
+If ($count -gt 0)
+{
+Copy-Item -Path $source_path -Destination $destination_path -Filter $file_name
+}
