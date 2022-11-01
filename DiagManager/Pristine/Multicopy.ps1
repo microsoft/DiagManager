9b36d904bd7519a2eb511b593c133565f9b93090
Kinye Yu <kennyyu2000@gmail.com>
2022-11-01 14:39:37 -0500

    Version 1.1 Multicopy
    1. Moved the Multicopy.ps1 to pristine folder.
    
    2. Fixed Named Instance bug and parsed out he instance name for the file name.
    
    3. Updated Custom Diag for SQLDIAG

---
+++ DiagManager/Pristine/Multicopy.ps1
@@ -0,0 +1,49 @@
+﻿param(
+  [Parameter(Position=0)]
+  [string]$sourcePath,
+  [Parameter(Position=1)]
+  [string]$destinationPath,
+  [Parameter(Position=2)]
+  [string]$serverName
+)
+
+function Get-InstanceNameOnly([string]$NetnamePlusInstance)
+{
+    try 
+    {
+        $selectedSqlInstance = $NetnamePlusInstance.Substring($NetnamePlusInstance.IndexOf("\") + 1)
+        return $selectedSqlInstance         
+    }
+    catch 
+    {
+        Write-Output "Exception in Get-InstanceNameOnly: $_.Exception.Message)"
+    }
+}
+
+if ($serverName -notlike '*\*')
+{
+  Return $serverName
+} 
+ else
+{
+  $serverName = Get-InstanceNameOnly -NetnamePlusInstance $serverName 
+}
+
+$errorFile = "c:\temp\copyFileError.txt"
+If (Test-Path $errorFile)
+{
+   Remove-Item $errorFile
+}
+
+Get-ChildItem $sourcePath -FILE | ForEach-Object { 
+		$newfileName = $serverName + "_" + $_.Name
+ 
+		$newfileName = Join-path $destinationPath $newfileName
+
+	Try { 
+				Copy-Item -Path $_.FullName -Destination $newFileName -ErrorAction Stop
+		}		
+	Catch {
+				$_.Exception.Message | Out-File -FilePath $errorFile -Append
+		}
+	}
